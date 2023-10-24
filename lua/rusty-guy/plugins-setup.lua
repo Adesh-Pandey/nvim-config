88c78052df6f1aae9eae13f9cb37f7e266fb9d46
Adesh-Pandey <adeshpandey836@gmail.com>
2023-10-25 01:09:45 +0545

    upgrade to lazy from packer

--- lua/rusty-guy/plugins-setup.lua
+++ lua/rusty-guy/plugins-setup.lua
@@ -1,102 +1,90 @@
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
-end 
-
-local packer_bootstrap = ensure_packer()
-
+vim.g.mapleader = " "
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath) 
 -- Autocommand that reloads neovim whenever you save to this file
 
-
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
-  augroup end
-]])
-
-local status, packer = pcall(require , "packer")
+local status, lazy= pcall(require , "lazy")
 if not status then
     return 
 end
 
-return packer.startup(function(use)
+return lazy.setup({
     
-    use("wbthomason/packer.nvim") -- package manager
-    use("bluz71/vim-nightfly-guicolors") -- theme
-    use("christoomey/vim-tmux-navigator") -- to navigate between split window
-    use("szw/vim-maximizer") -- toggle size of split window
+    "bluz71/vim-nightfly-guicolors", -- theme
+    "christoomey/vim-tmux-navigator", -- to navigate between split window
+    "szw/vim-maximizer", -- toggle size of split window
 
-    use("tpope/vim-surround") -- wrap with characters
-    use("vim-scripts/ReplaceWithRegister") -- replace by coping
+    "tpope/vim-surround", -- wrap with characters
+    "vim-scripts/ReplaceWithRegister", -- replace by coping
 
-    use("numToStr/Comment.nvim") -- commenting
+    "numToStr/Comment.nvim", -- commenting
 
-    use("nvim-tree/nvim-tree.lua") -- explorer
-    use("kyazdani42/nvim-web-devicons") -- icons
+    "nvim-tree/nvim-tree.lua", -- explorer
+    "kyazdani42/nvim-web-devicons", -- icons
     
   -- fuzzy finding w/ telescope
-  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
-  use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
-
+{ "nvim-telescope/telescope.nvim", dependencies ={ "tsakirist/telescope-lazy.nvim", "nvim-lua/plenary.nvim",
+} },
   -- autocompletion
-  use("hrsh7th/nvim-cmp") -- completion plugin
-  use("hrsh7th/cmp-buffer") -- source for text in buffer
-  use("hrsh7th/cmp-path") -- source for file system paths
+  "hrsh7th/nvim-cmp", -- completion plugin
+  "hrsh7th/cmp-buffer", -- source for text in buffer
+  "hrsh7th/cmp-path", -- source for file system paths
     
 
   -- snippets
-  use("L3MON4D3/LuaSnip") -- snippet engine
-  use("saadparwaiz1/cmp_luasnip") -- for autocompletion
-  use("rafamadriz/friendly-snippets") -- useful snippets
+  "L3MON4D3/LuaSnip", -- snippet engine
+  "saadparwaiz1/cmp_luasnip", -- for autocompletion
+  "rafamadriz/friendly-snippets", -- useful snippets
 
   -- managing & installing lsp servers, linters & formatters
-  use("williamboman/mason.nvim") -- in charge of managing lsp servers, linters & formatters
-  use("williamboman/mason-lspconfig.nvim") -- bridges gap b/w mason & lspconfig
+  "williamboman/mason.nvim", -- in charge of managing lsp servers, linters & formatters
+  "williamboman/mason-lspconfig.nvim", -- bridges gap b/w mason & lspconfig
 
   -- configuring lsp servers
-  use("neovim/nvim-lspconfig") -- easily configure language servers
-  use("hrsh7th/cmp-nvim-lsp") -- for autocompletion
-  use({
+  "neovim/nvim-lspconfig", -- easily configure language servers
+  "hrsh7th/cmp-nvim-lsp", -- for autocompletion
+  {
     "glepnir/lspsaga.nvim",
     branch = "main",
-    requires = {
+    dependencies= {
       { "nvim-tree/nvim-web-devicons" },
       { "nvim-treesitter/nvim-treesitter" },
     },
-  }) -- enhanced lsp uis
-  use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
-  use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
+  }, -- enhanced lsp uis
+  "jose-elias-alvarez/typescript.nvim", -- additional functionality for typescript server (e.g. rename file & update imports)
+  "onsails/lspkind.nvim", -- vs-code like icons for autocompletion
 
   -- formatting & linting
-  use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
-  use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
+  "jose-elias-alvarez/null-ls.nvim", -- configure formatters & linters
+  "jayp0521/mason-null-ls.nvim", -- bridges gap b/w mason & null-ls
 
   -- treesitter configuration
-  use({
+  {
     "nvim-treesitter/nvim-treesitter",
     run = function()
       local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
       ts_update()
     end,
-  })
+        dependencies={
+        "windwp/nvim-ts-autotag",}
+  },
   
   -- auto closing
-  use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
-  use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
+  "windwp/nvim-autopairs", -- autoclose parens, brackets, quotes, etc...
 
   -- git integration
-  use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
+  "lewis6991/gitsigns.nvim", -- show line modifications on left hand side
 
-    use("nvim-lualine/lualine.nvim")
-    if packer_bootstrap then
-        require("packer").sync()
-    end 
-end)
+    "nvim-lualine/lualine.nvim",
+   j})
  
--- lua/rusty-guy/plugins/_telescope.lua
+++ lua/rusty-guy/plugins/_telescope.lua
@@ -1,27 +1,35 @@
--- import telescope plugin safely
-local telescope_setup, telescope = pcall(require, "telescope")
-if not telescope_setup then
-  return
-end
-
--- import telescope actions safely
-local actions_setup, actions = pcall(require, "telescope.actions")
-if not actions_setup then
-  return
-end
-
-telescope.load_extension("fzf")
--- configure telescope
-telescope.setup({
-  -- configure custom mappings
-  defaults = {
-    mappings = {
-      i = {
-        ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-        ["<C-j>"] = actions.move_selection_next, -- move to next result
-        ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
+require("telescope").setup({
+  extensions = {
+    lazy = {
+      -- Optional theme (the extension doesn't set a default theme)
+      theme = "ivy",
+      -- Whether or not to show the icon in the first column
+      show_icon = true,
+      -- Mappings for the actions
+      mappings = {
+        open_in_browser = "<C-o>",
+        open_in_file_browser = "<M-b>",
+        open_in_find_files = "<C-f>",
+        open_in_live_grep = "<C-g>",
+        open_in_terminal = "<C-t>",
+        open_plugins_picker = "<C-b>", -- Works only after having called first another action
+        open_lazy_root_find_files = "<C-r>f",
+        open_lazy_root_live_grep = "<C-r>g",
       },
+      -- Configuration that will be passed to the window that hosts the terminal
+      -- For more configuration options check 'nvim_open_win()'
+      terminal_opts = {
+        relative = "editor",
+        style = "minimal",
+        border = "rounded",
+        title = "Telescope lazy",
+        title_pos = "center",
+        width = 0.5,
+        height = 0.5,
+      },
+      -- Other telescope configuration options
     },
   },
 })
 
+require("telescope").load_extension "lazy"
